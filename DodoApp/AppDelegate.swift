43c1f849056e34510811fff556cfa0fe3f24bb73
Dillon Fernando <dfernando1120@gmail.com>
2017-01-19 17:33:09 -0500

    Dodo app swift code

--- DodoApp/AppDelegate.swift
+++ DodoApp/AppDelegate.swift
@@ -20,19 +20,16 @@ import UIKit
 
 
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
     
     var window: UIWindow?
     
-    
+
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         return true
     }
     
-    
     func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable: Any]) {
     }
-
     
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
@@ -56,7 +53,5 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
-
-
 }
 
